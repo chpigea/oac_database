722f4a351b4ab02847002c75e5b435ce7dba5947
Christian R. Picone <ch.picone@gmail.com>
2025-07-13 16:52:38 +0200

    update

--- src/migrations/0000000000003-first-user.sql
+++ src/migrations/0000000000003-first-user.sql
@@ -2,6 +2,9 @@
 ALTER TABLE USERS DROP COLUMN token;
 ALTER TABLE USERS ADD COLUMN is_admin BOOLEAN DEFAULT FALSE;
 ---------------------------------------------------------------
-INSERT INTO USERS(name, surname, username, email, password, valid, is_admin)
-VALUES('igea', 'igea', 'igea', 'info@igea-soluzioni.it', md5('@igea#'), 1, TRUE);
+ALTER TABLE USERS DROP COLUMN valid;
+ALTER TABLE USERS ADD COLUMN is_active BOOLEAN DEFAULT TRUE;
+---------------------------------------------------------------
+INSERT INTO USERS(name, surname, username, email, password, is_admin)
+VALUES('igea', 'igea', 'igea', 'info@igea-soluzioni.it', md5('@igea#'), TRUE);
 ---------------------------------------------------------------
