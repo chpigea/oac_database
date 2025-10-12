0f8ea6eebbf772177c3c5162e2aed494f7b9c0ce
Christian R. Picone <ch.picone@gmail.com>
2025-10-12 12:06:05 +0200

    add CLI prompt

--- src/migrations/0000000000003-first-user.sql
+++ src/migrations/0000000000003-first-user.sql
@@ -1,9 +1,9 @@
 ---------------------------------------------------------------
-ALTER TABLE USERS DROP COLUMN token;
-ALTER TABLE USERS ADD COLUMN is_admin BOOLEAN DEFAULT FALSE;
+ALTER TABLE USERS DROP COLUMN IF EXISTS token;
+ALTER TABLE USERS ADD COLUMN IF NOT EXISTS is_admin BOOLEAN DEFAULT FALSE;
 ---------------------------------------------------------------
-ALTER TABLE USERS DROP COLUMN valid;
-ALTER TABLE USERS ADD COLUMN is_active BOOLEAN DEFAULT TRUE;
+ALTER TABLE USERS DROP COLUMN IF EXISTS valid;
+ALTER TABLE USERS ADD COLUMN IF NOT EXISTS is_active BOOLEAN DEFAULT TRUE;
 ---------------------------------------------------------------
 INSERT INTO USERS(name, surname, username, email, password, is_admin)
 VALUES('igea', 'igea', 'igea', 'info@igea-soluzioni.it', md5('@igea#'), TRUE);
